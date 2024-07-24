a0527725579c41e11f4c2334124f0eb5bc4ac2eb
Augusto Pascutti <augusto.hp@gmail.com>
2024-07-24 18:57:04 -0300

    Removes function signature completion
    This annoys most of the time while you are writing something. Maybe
    being able to configure would make the experience better but the
    defaults suck. And I didn't have the patience to go looking how to
    improve it.

--- home/.config/nvim/lua/nvim-cmp.lua
+++ home/.config/nvim/lua/nvim-cmp.lua
@@ -136,7 +136,6 @@ cmp.setup({
   -- https://github.com/hrsh7th/nvim-cmp/wiki/List-of-sources
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'nvim_lsp_signature_help' },
     { name = 'vsnip' },
     { name = 'emoji' },
   }, {
